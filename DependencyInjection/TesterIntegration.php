8ce418712759896bb5c2c967ca8438d556031142
Martin Poirier Théorêt <mpoiriert@gmail.com>
2024-06-28 14:08:20 -0400

    [Tester] Use new draw/dependency-injection component

---
+++ DependencyInjection/TesterIntegration.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Draw\Component\Tester\DependencyInjection;
+
+use Draw\Component\DependencyInjection\Integration\IntegrationInterface;
+use Draw\Component\DependencyInjection\Integration\IntegrationTrait;
+use Draw\Component\Tester\Command\TestsCoverageCheckCommand;
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
+
+class TesterIntegration implements IntegrationInterface
+{
+    use IntegrationTrait;
+
+    public function getConfigSectionName(): string
+    {
+        return 'tester';
+    }
+
+    public function load(array $config, PhpFileLoader $loader, ContainerBuilder $container): void
+    {
+        $container->setDefinition(
+            'draw.tester.command.tests_coverage_check_command',
+            new Definition(TestsCoverageCheckCommand::class)
+        )
+            ->setAutoconfigured(true)
+            ->setAutowired(true);
+
+        $container->setAlias(TestsCoverageCheckCommand::class, 'draw.tester.command.tests_coverage_check_command');
+    }
+
+    public function addConfiguration(ArrayNodeDefinition $node): void
+    {
+        // nothing to do
+    }
+}
---
+++ Tests/DependencyInjection/TesterIntegrationTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Draw\Component\Tester\Tests\DependencyInjection;
+
+use Draw\Component\DependencyInjection\Integration\IntegrationInterface;
+use Draw\Component\DependencyInjection\Integration\Test\IntegrationTestCase;
+use Draw\Component\DependencyInjection\Integration\Test\ServiceConfiguration;
+use Draw\Component\Tester\Command\TestsCoverageCheckCommand;
+use Draw\Component\Tester\DependencyInjection\TesterIntegration;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+/**
+ * @property TesterIntegration $integration
+ */
+#[CoversClass(TesterIntegration::class)]
+class TesterIntegrationTest extends IntegrationTestCase
+{
+    public function createIntegration(): IntegrationInterface
+    {
+        return new TesterIntegration();
+    }
+
+    public function getConfigurationSectionName(): string
+    {
+        return 'tester';
+    }
+
+    public function getDefaultConfiguration(): array
+    {
+        return [];
+    }
+
+    public static function provideTestLoad(): iterable
+    {
+        yield [
+            [],
+            [
+                new ServiceConfiguration(
+                    'draw.tester.command.tests_coverage_check_command',
+                    [
+                        TestsCoverageCheckCommand::class,
+                    ]
+                ),
+            ],
+        ];
+    }
+}
